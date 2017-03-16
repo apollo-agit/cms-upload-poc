1fa7e30bba71b9dbd4f0110385b0dde2de8b82bc
Ian Hamilton <ian.hamilton.it@gmail.com>
2017-03-16 10:18:24 -0700

    added the name to the repo

--- server/api-router.js
+++ server/api-router.js
@@ -1,7 +1,6 @@
 express = require('express');
 var serverconfig = require('./server.config');
-var multer  = require('multer')
-var upload = multer({dest: serverconfig.filedir});
+var multer  = require('multer');
 var apiRouter = express.Router(); 
 var alfrescoJsApi = require('./cms-client');
 var fs = require('fs');
@@ -9,6 +8,18 @@ var fs = require('fs');
 var db = require('node-localdb');
 var filedb = db(serverconfig.dbdir + 'file.json');
 var FileEnity = require('./file-entity');
+
+
+
+var storage = multer.diskStorage({
+  destination: serverconfig.filedir,
+  filename: function (req, file, cb) {
+  	console.log(req.body);
+    cb(null, serverconfig.user_first_name + serverconfig.user_last_name + '-' + Date.now());
+  }
+})
+
+var upload = multer({ storage: storage })
 
 apiRouter.route('/upload') 
 	.post(upload.single('file'), function(req, res) {
